3142d44d9bae21124c54f23b365f9b6ac71f2bf7
NazimRajabov <90709382+NazimRajabov@users.noreply.github.com>
2021-09-19 18:23:30 +0400

    login parol

---
+++ login parol/login parol/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace login_parol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int giris_haqqi = 3;
+
+            while (true)
+            {
+                Console.WriteLine("Logini Daxil Edin: ");
+                string login = Console.ReadLine();
+
+                Console.WriteLine("Sifreni Daxil Edin: ");
+                string sifre = Console.ReadLine();
+                if (login == "Nazim Rajabov" && sifre == "224466nr")
+                {
+                    Console.WriteLine("Hesabiniza muveffeqiyyetle giris edildi!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Daxil etdiyiniz melumatlar duzgun deyil");
+                    if (giris_haqqi >= 0)
+                    {
+                        giris_haqqi--;
+                    }
+                    if (giris_haqqi == 0)
+                    {
+                        Console.WriteLine("Girisiniz Muveqqeti Bloklanmisdir.");
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
